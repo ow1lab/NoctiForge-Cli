66e4f8ce7bd885d1591cd2751dafb1b2b49f2ad3
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2026-01-13 18:56:11 +0100

    feat: move the cli over to this repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+
+mod command;
+
+mod api {
+    pub mod registry {
+        tonic::include_proto!("noctiforge.registry");
+    }
+    pub mod controlplane {
+        tonic::include_proto!("noctiforge.controlplane");
+    }
+    pub mod worker {
+        tonic::include_proto!("noctiforge.worker");
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    command::run().await
 }
